b5c16b3f4a64157d586d3d70de324bffaa301a61
CalCraven <nicholas.c.craven@vanderbilt.edu>
2023-09-04 09:31:34 -0500

    add submodules to top __init__.py

--- surface_coatings/__init__.py
+++ surface_coatings/__init__.py
@@ -1,1 +1,5 @@
 """__init__ file for surface_coatings module."""
+from .monolayer import Monolayer, DualMonolayer
+from .solvated_monolayer import SolvatedMonolayer
+from .surfaces.silica_interface import SilicaInterface
+from .chains.alkylsilane import Alkylsilane
