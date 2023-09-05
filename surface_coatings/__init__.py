5928c3c340db002fc52928e9a73a39a9f5ef6276
CalCraven <nicholas.c.craven@vanderbilt.edu>
2023-09-05 07:43:53 -0500

    add packages to __init__.py

--- surface_coatings/__init__.py
+++ surface_coatings/__init__.py
@@ -2,4 +2,9 @@
 from .monolayer import Monolayer, DualMonolayer
 from .solvated_monolayer import SolvatedMonolayer
 from .surfaces.silica_interface import SilicaInterface
+from .surface.silicon_interface import CrystalineSilicon
+from .surfaces.silica_interface_carve import SilicaInterfaceCarve
 from .chains.alkylsilane import Alkylsilane
+from .chains.silane_polymer import SilanePolymer
+from .chains.nbdac_polymer import fmNBDAC, pNBDAC
+from .chains.vbc_polymer import VBCPolymer
